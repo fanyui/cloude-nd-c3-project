ce5d8cdda818232fce503ba8f3144a68d0e3dba6
Harisu Fanyui <fanyuiharisu@gmail.com>
2020-07-12 17:32:48 +0100

    update memstash code for backend url persisting.

--- bkmemstash.sh
+++ bkmemstash.sh
@@ -2,3 +2,4 @@
 ipaddress=$( tail -n 1 inventory.txt )
 echo $ipaddress
 curl -H "Content-Type: text/plain" -H "token: 347407f8-3d4a-4990-98c5-3b96142561b4" --request PUT --data $ipaddress https://api.memstash.io/values/ipaddress
+curl -H "Content-Type: text/plain" -H "token: 347407f8-3d4a-4990-98c5-3b96142561b4" --request PUT --data ipaddress https://api.memstash.io/values/backend
