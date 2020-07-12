ec8e72af6c1065e81e1c30a0043eea4a75ec02a0
Harisu Fanyui <fanyuiharisu@gmail.com>
2020-07-12 10:37:16 +0100

    Added job for getting instance ip address and storing in the memstash api as well as configure the instance

---
+++ bkmemstash.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+ipaddress=$( tail -n 1 inventory.txt )
+echo $ipaddress
+curl -H "Content-Type: text/plain" -H "token: 347407f8-3d4a-4990-98c5-3b96142561b4" --request PUT --data $ipaddress https://api.memstash.io/values/ipaddress
