305c3bc577007f18db8868d53f6eef175d13860d
Harisu Fanyui <fanyuiharisu@gmail.com>
2020-07-14 17:31:36 +0100

    changed the executor and also remove the .txt from inventory file "

--- bkmemstash.sh
+++ bkmemstash.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ipaddress=$( tail -n 1 inventory.txt )
+ipaddress=$( tail -n 1 inventory )
 echo $ipaddress
 curl -H "Content-Type: text/plain" -H "token: 347407f8-3d4a-4990-98c5-3b96142561b4" --request PUT --data $ipaddress https://api.memstash.io/values/ipaddress
 curl -H "Content-Type: text/plain" -H "token: 347407f8-3d4a-4990-98c5-3b96142561b4" --request PUT --data ipaddress https://api.memstash.io/values/backend
