d7090604976c0e51ef52d787c12dcb1e4e25f7aa
Harisu Fanyui <fanyuiharisu@gmail.com>
2020-07-18 16:18:59 +0100

    removed the job for storing old bucket arn to memstash

--- bkmemstash.sh
+++ bkmemstash.sh
@@ -2,3 +2,5 @@
 ipaddress=$( tail -n 1 inventory )
 echo $ipaddress
 curl -H "Content-Type: text/plain" -H "token: 347407f8-3d4a-4990-98c5-3b96142561b4" --request PUT --data $ipaddress https://api.memstash.io/values/backend
+
+curl -H "Content-Type:text/plain" -H "token:347407f8-3d4a-4990-98c5-3b96142561b4" --request PUT --data "arn:aws:s3:::superpower-${ID}" https://api.memstash.io/values/frontend)
