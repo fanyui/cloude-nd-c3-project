3094634f13f8fbc682f08664751ec42f09565e9e
Harisu Fanyui <fanyuiharisu@gmail.com>
2020-07-29 14:19:21 +0100

    Refactor syntax error

--- extract_ip_address.sh
+++ extract_ip_address.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ip=$(curl -H "token: 347407f8-3d4a-4990-98c5-3b96142561b4" --request GET https://api.memstash.io/values/backend )
 echo "[all]" > inventory
-echo  "${$ip} ansible_user=ubuntu" >> inventory
+echo  "${ip} ansible_user=ubuntu" >> inventory
