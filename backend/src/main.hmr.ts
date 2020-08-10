a72799217457ffca1b403d1eaeb316b5299dc47c
Harisu Fanyui <fanyuiharisu@gmail.com>
2020-08-10 03:30:29 +0100

    Added command to roll back plus also added some job to clean up the stack when everything is done

--- backend/src/main.hmr.ts
+++ backend/src/main.hmr.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from 'modules/app/app.module';
+import { AppModule } from './modules/app/app.module';
 import { ConfigService } from './modules/config/config.service';
 
 declare const module: any;
